d58aa1f2c67a5e1916bc9099d9c7a4d0cb6945a1
yonmoyonmo <yonmosound@gmail.com>
2026-04-14 21:33:25 +0900

    Initial Commit

---
+++ pixel-art-editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pixel-art-editor
+//
+//  Created by yonmo on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pixel-art-editor/pixel_art_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_art_editorApp.swift
+//  pixel-art-editor
+//
+//  Created by yonmo on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct pixel_art_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
